7318fe6059c778348504d3ee66b40aed28d2cf22
Iurii Kurinnoi <yur4kur@icloud.com>
2023-06-01 14:02:40 +0400

    Initial Commit

---
+++ Lesson 2.01_Constraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.01_Constraints
+//
+//  Created by Юрий Куринной on 01.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
